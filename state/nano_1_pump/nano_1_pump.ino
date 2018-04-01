58b324568804940605f9b5718507f2b189ffc450
mdew192837 <justin.d128@gmail.com>
2018-04-01 17:13:09 -0500

    Add Nano 1 (Pump) Code

---
+++ state/nano_1_pump/nano_1_pump.ino
@@ -0,0 +1,60 @@
+/* Sweep
+  by BARRAGAN <http://barraganstudio.com>
+  This example code is in the public domain.
+
+  modified 8 Nov 2013
+  by Scott Fitzgerald
+  http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+// Pins we need
+int READ_PIN = 2;
+int CIRCUIT_PIN = 3;
+int PUMP_PIN = 9;
+int SEND_PIN = 13;
+
+// Speeds
+int SLOW_SPEED = 50;
+int PUMP_RUN_TIME = 5000;
+
+// Booleans
+boolean STARTED = false;
+boolean FINISHED = false;
+
+void setup() {
+  Serial.begin(9600);
+  // Set up pin 2 as HIGH
+  pinMode(RECEIVE_PIN, INPUT_PULLUP);
+  pinMode(SEND_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(150);
+}
+
+void loop() {
+  while ( !STARTED && !FINISHED ) {
+    // Make sure we haven't ran the pump
+    // AND
+    // The pullup resistor is now LOW
+    if ( !STARTED && !digitalRead(RECEIVE_PIN) ) {
+      // Speed ranges from 0-255
+      // We'll just do 50 as low
+      analogWrite(PUMP_PIN, SLOW_SPEED);
+      delay(PUMP_RUN_TIME);
+      // Pump off
+      analogWrite(PUMP_PIN, 0);
+      // Update boolean so we don't run the pump again
+      STARTED = true;
+    }
+  }
+  // Make sure pump has run and we haven't triggered already
+  if ( STARTED && !FINISHED ) {
+    // Note to self: If for some reason it doesn't work...
+    // Just take out the if! :)
+    if ( !digitalRead(CIRCUIT_PIN) ) {
+      // Send dat signal!
+      digitalWrite(SEND_PIN, HIGH);
+      FINISHED = true;
+    }
+  }
+}
