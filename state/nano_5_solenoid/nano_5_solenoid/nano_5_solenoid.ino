1dc07c8b310a30bc7008eb27526c13df59d50d54
mdew192837 <justin.d128@gmail.com>
2018-04-03 15:14:15 -0500

    Add nano 5 (solenoid) code

---
+++ state/nano_5_solenoid/nano_5_solenoid/nano_5_solenoid.ino
@@ -0,0 +1,51 @@
+/*
+ * WSHS Science Olympiad State 2018
+ * @author Justin Du (MtDu)
+*/
+
+// Pins we need
+int RECEIVE_PIN = 2;
+int SECOND_RECEIVE_PIN = 4;
+int SOLENOID = 9;
+int SEND_PIN = 13;
+
+// Speeds
+int HIGH_SPEED = 255;
+int SOLENOID_RUN_TIME = 500;
+
+// Booleans
+boolean TRIGGERED = false;
+boolean ACTION_COMPLETE = false;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RECEIVE_PIN, INPUT_PULLUP);
+  pinMode(SECOND_RECEIVE_PIN, INPUT_PULLUP);
+  pinMode(SOLENOID, OUTPUT);
+  pinMode(SEND_PIN, OUTPUT);
+}
+
+void loop() {
+  // Until we want to trigger it
+  digitalWrite(13, HIGH);
+
+  /*
+   * How this works...
+   * 1. Make sure we haven't triggered before and we have a signal on 2
+   * 2. Run the solenoid (which starts the marble action...)
+   * 3. Stop solenoid.
+   * 4. Once action is complete, send the signal to pin 13
+   */
+  if ( !digitalRead(RECEIVE_PIN) && !TRIGGERED ) {
+    analogWrite(SOLENOID, HIGH_SPEED);
+    delay(SOLENOID_RUN_TIME);
+    analogWrite(9, 0);
+    TRIGGERED = true;
+  }
+
+  if ( !ACTION_COMPLETE && TRIGGERED && !digitalRead(SECOND_RECEIVE_PIN) {
+    digitalWrite(13, LOW);
+    // So it doesn't run again...
+    ACTION_COMPLETE = true;
+  }
+}
